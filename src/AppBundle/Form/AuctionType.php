a6a931e16b353bf881bc368433aa07f0ebae5bcb
Michał Derkacz <derkaczes@gmail.com>
2019-11-14 17:02:27 +0100

    Added new fields in the product adding form

--- src/AppBundle/Form/AuctionType.php
+++ src/AppBundle/Form/AuctionType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AuctionType extends AbstractType {
@@ -17,6 +18,8 @@ class AuctionType extends AbstractType {
             ->add("title", TextType::class, ['label' => "Tytuł"])
             ->add("description", TextareaType::class, ['label' => "Opis"])
             ->add("price", NumberType::class, ['label' => "Cena"])
+            ->add("startingPrice", NumberType::class, ['label' => "Cena wywoławcza"])
+            ->add("expiresAt", DateTimeType::class, ['label' => "Data zakończenia"])
             ->add("submit", SubmitType::class, ['label' => "Zapisz"]);
     }
 
