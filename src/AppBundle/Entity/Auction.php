76c91b205c059b2685316948baad6f16ddfd7bce
Michał Derkacz <derkaczes@gmail.com>
2019-11-14 16:55:08 +0100

    Added new columns in database

--- src/AppBundle/Entity/Auction.php
+++ src/AppBundle/Entity/Auction.php
@@ -12,6 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Auction
 {
+    const STATUS_ACTIVE = "active";
+    const STATUS_FINISHED = "finished";
+    const STATUS_CANCELLED = "cancelled";
+    
     /**
      * @var int
      *
@@ -36,11 +40,46 @@ class Auction
     private $description;
 
     /**
-     * @var string
+     * @var float
      *
      * @ORM\Column(name="price", type="decimal", precision=10, scale=2)
      */
     private $price;
+
+    /**
+     * @var float
+     * 
+     * @ORM\Column(name="starting_price", type="decimal", precision=10, scale=2)
+     */
+    private $startingPrice;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(name="expires_at", type="datetime")
+     */
+    private $expiresAt;
+
+     /**
+     * @var string
+     * 
+     * @ORM\Column(name="status", type="string", length=10)
+     */
+    private $status;
 
 
     /**
@@ -104,7 +143,7 @@ class Auction
     /**
      * Set price
      *
-     * @param string $price
+     * @param float $price
      *
      * @return Auction
      */
@@ -118,11 +157,131 @@ class Auction
     /**
      * Get price
      *
-     * @return string
+     * @return float
      */
     public function getPrice()
     {
         return $this->price;
+    }
+
+    /**
+     * Set startingPrice
+     *
+     * @param float $startingPrice
+     *
+     * @return Auction
+     */
+    public function setStartingPrice($startingPrice)
+    {
+        $this->startingPrice = $startingPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get startingPrice
+     *
+     * @return string
+     */
+    public function getStartingPrice()
+    {
+        return $this->startingPrice;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Auction
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+     /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Auction
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set expiresAt
+     *
+     * @param \DateTime $expiresAt
+     *
+     * @return Auction
+     */
+    public function setExpiresAt(\DateTime $expiresAt)
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+
+    /**
+     * Get expiresAt
+     *
+     * @return \DateTime
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * Set status
+     *
+     * @param string $status
+     *
+     * @return Auction
+     */
+    public function setStatus(\DateTime $status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
     }
 }
 
