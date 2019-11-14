2277b86ac9a3f1b60107a1f1c0b18932bb4fac79
Michał Derkacz <derkaczes@gmail.com>
2019-11-14 12:53:59 +0100

    Created function addAction

--- src/AppBundle/Controller/AuctionController.php
+++ src/AppBundle/Controller/AuctionController.php
@@ -5,6 +5,11 @@ use AppBundle\Entity\Auction;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class AuctionController extends Controller {
     /**
@@ -18,12 +23,29 @@ class AuctionController extends Controller {
 
         return $this->render("Auction/index.html.twig", ["auctions" => $auctions]);
     }
+
     /**
      * @Route("/{id}", name="auction_details")
-     * 
+     * s
      * @param $id
      */
     public function detailsAction($id) {
         return $this->render("Auction/details.html.twig");
+    }
+
+    /**
+     * @Route("/auction/add", name="auction_add")
+     * 
+     * @return Response
+     */
+    public function addAction() {
+        $auction = new Auction();
+        $form = $this->createFormBuilder($auction)
+            ->add("title", TextType::class)
+            ->add("description", TextareaType::class)
+            ->add("price", NumberType::class)
+            ->add("submit", SubmitType::class)
+            ->getForm();
+        return $this->render("Auction/add.html.twig", ["form" => $form->createView()]);
     }
 }
