d0e6e0ab10caf1971aab3ccd8c6245e42264b70d
Michał Derkacz <derkaczes@gmail.com>
2019-11-15 10:55:09 +0100

    Created buy auction

---
+++ src/AppBundle/Controller/OfferController.php
@@ -0,0 +1,35 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Auction;
+use AppBundle\Entity\Offer;
+
+class OfferController extends Controller {
+
+    /**
+     * @Route("/auction/buy/{id}", name="offer_buy", methods={"POST"})
+     * 
+     * @param Auction $auction
+     * 
+     * @return RedirectResponse
+     */
+    public function buyAction(Auction $auction) {
+        $offer = new Offer();
+        $offer
+            ->setAuction($auction)
+            ->setType(Offer::TYPE_BUY)
+            ->setPrice($auction->getPrice());
+
+        $auction->setStatus(Auction::STATUS_FINISHED);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($auction);
+        $entityManager->persist($offer);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("auction_details", ["id" => $auction->getId()]);
+    }
+}
